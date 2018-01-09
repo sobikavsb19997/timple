13216cc561d554cb04ffab328f2acf88d141070f
sobikavsb19997 <31503937+sobikavsb19997@users.noreply.github.com>
2018-01-09 16:03:49 +0530

    Create possitive.c

---
+++ possitive.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter a integer");
+scanf("%d",&n);
+if(n==0)
+{
+printf("the integer is zero");
+}
+else if(n<0)
+{
+printf("it is negative");
+}
+else
+{
+printf("it is negative");
+}
+}
