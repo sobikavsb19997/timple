55fabe47b1b3b853e6ccf3b402b9f39d3299705c
sobikavsb19997 <31503937+sobikavsb19997@users.noreply.github.com>
2018-01-10 22:02:45 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter how many times you want  to print hello");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+printf("HELLO");
+}
+getch();
+}
