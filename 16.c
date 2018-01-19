2280ac468ee127c66ecec350b05a991910d049dd
sobikavsb19997 <31503937+sobikavsb19997@users.noreply.github.com>
2018-01-19 15:54:49 +0530

    Create 16.c

---
+++ 16.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int
+for(i=1;i<=10;i++)
+{
+for(i=2;i<=n/2;i++)
+{
+if(n%2==0)
+{
+flag=1;
+break;
+}
+if(flag=0)
+{
+printf("it is prime");
+}
+else
+{
+printf("composite");
+}
+}
+}
+}
