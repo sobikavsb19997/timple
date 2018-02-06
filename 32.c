31a2a0bd9c397e3fb408742d8e55060675ca2382
sobikavsb19997 <31503937+sobikavsb19997@users.noreply.github.com>
2018-02-06 14:19:10 +0530

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+ char a[n];
+ int n,count=1;
+ printf("enter a sentence");
+ scanf("%d",s);
+ printf("enter the limit");
+ scanf("%d",n);
+ for(i=0;i<=n;i++)
+ {
+ if(a[i]==" ")
+ {
+ count=count+1;
+ }
+ printf("number of words=%d",count);
+ ]
+ }
