715fed5a9c95c34eefbc58e48089f28cb629ef8f
sobikavsb19997 <31503937+sobikavsb19997@users.noreply.github.com>
2018-02-09 08:40:11 +0000

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+  char s[20];
+  int k,i;
+   printf("enter a string");
+  scanf("%s",s);
+  printf("enter the limit");
+  scanf("%d",&k);
+  for(i=0;i<k;i++)
+  {
+    printf("\n%s",s);
+    
+  }
+  
+}
