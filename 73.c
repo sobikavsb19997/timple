e2c1eaee927dcbb2dd4c74642813445ea44f4557
sobikavsb19997 <31503937+sobikavsb19997@users.noreply.github.com>
2018-03-09 16:52:11 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+  int a,l,h;
+  printf("enter a number");
+  scanf("%d",&a);
+  printf("enter limits l,h");
+  scanf("%d%d",&l,&r);
+  if(a>l&&a<h)
+  {
+    printf("yes");
+  }
+  else
+  {
+    printf("no");
+  }
+}
